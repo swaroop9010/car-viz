b307994382ed26a82b8ee132d7be495a69400f71
Sai swaroop <104573154+swaroop9010@users.noreply.github.com>
2025-04-11 17:17:32 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function showChart(id) {
-  const charts = ["lineChart", "scatterPlot", "barChart"];
-  charts.forEach(chart => {
-    document.getElementById(chart).style.display = chart === id ? "block" : "none";
-  });
-}
