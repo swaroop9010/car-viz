45df7d9dc55de6dc8e0c6e361ec1d57edb897720
Sai swaroop <104573154+swaroop9010@users.noreply.github.com>
2025-04-11 17:13:23 -0400

    Update scatter-plot.js

--- scatter-plot.js
+++ scatter-plot.js
@@ -34,4 +34,16 @@ d3.csv("a1-cars.csv").then(data => {
 
   svg.append("text").attr("x", width / 2).attr("y", -5)
     .attr("text-anchor", "middle").style("font-size", "16px").text("Horsepower vs. MPG");
+
+  svg.append("g")
+   .attr("transform", `translate(0,${height})`)
+   .call(d3.axisBottom(x))
+   .selectAll("path,line")
+   .attr("stroke", "black");
+
+svg.append("g")
+   .call(d3.axisLeft(y))
+   .selectAll("path,line")
+   .attr("stroke", "black");
+
 });
