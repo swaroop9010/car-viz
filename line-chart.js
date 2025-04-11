f678a97092f84d4e4fe9b12235fdf3f92fd54ce4
Sai swaroop <104573154+swaroop9010@users.noreply.github.com>
2025-04-11 17:23:00 -0400

    Add files via upload

---
+++ line-chart.js
@@ -0,0 +1,52 @@
+
+d3.csv("a1-cars.csv").then(data => {
+  data.forEach(d => {
+    d["Model Year"] = +d["Model Year"];
+    d.MPG = +d.MPG;
+  });
+
+  const nested = d3.groups(data, d => d["Origin"]);
+  const margin = {top: 40, right: 80, bottom: 50, left: 60},
+        width = 800 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+  const svg = d3.select("#lineChart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  const x = d3.scaleLinear().domain([70, 82]).range([0, width]);
+  const y = d3.scaleLinear().domain([0, d3.max(data, d => d.MPG)]).range([height, 0]);
+  const color = d3.scaleOrdinal(d3.schemeSet2);
+
+  svg.append("g")
+     .attr("transform", `translate(0,${height})`)
+     .attr("class", "axis")
+     .call(d3.axisBottom(x).tickFormat(d => 1900 + d));
+
+  svg.append("g")
+     .attr("class", "axis")
+     .call(d3.axisLeft(y));
+
+  const line = d3.line()
+    .defined(d => !isNaN(d.MPG))
+    .x(d => x(d["Model Year"]))
+    .y(d => y(d.MPG));
+
+  nested.forEach(([origin, values]) => {
+    svg.append("path")
+      .datum(values)
+      .attr("fill", "none")
+      .attr("stroke", color(origin))
+      .attr("stroke-width", 2)
+      .attr("d", line);
+
+    svg.append("text")
+      .attr("x", width - 60)
+      .attr("y", y(values[values.length - 1].MPG))
+      .attr("fill", color(origin))
+      .text(origin);
+  });
+});
---
+++ scatter-plot.js
@@ -0,0 +1,41 @@
+
+d3.csv("a1-cars.csv").then(data => {
+  data = data.filter(d => d.MPG && d.Horsepower);
+  data.forEach(d => {
+    d.Horsepower = +d.Horsepower;
+    d.MPG = +d.MPG;
+  });
+
+  const margin = {top: 20, right: 30, bottom: 50, left: 60},
+        width = 800 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+  const svg = d3.select("#scatterPlot")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  const x = d3.scaleLinear().domain(d3.extent(data, d => d.Horsepower)).range([0, width]);
+  const y = d3.scaleLinear().domain(d3.extent(data, d => d.MPG)).range([height, 0]);
+
+  svg.append("g")
+     .attr("transform", `translate(0,${height})`)
+     .attr("class", "axis")
+     .call(d3.axisBottom(x));
+
+  svg.append("g")
+     .attr("class", "axis")
+     .call(d3.axisLeft(y));
+
+  svg.selectAll("circle")
+    .data(data)
+    .enter().append("circle")
+    .attr("cx", d => x(d.Horsepower))
+    .attr("cy", d => y(d.MPG))
+    .attr("r", 4)
+    .attr("fill", "steelblue")
+    .append("title")
+    .text(d => `${d.Car} (${d.MPG} MPG)`);
+});
